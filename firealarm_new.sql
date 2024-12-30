bbb9af6da15be4e6f3d7e3d6a48965ee16aad9a2
Deepak Tripathi <deepak.codepix@gmail.com>
2024-12-30 16:09:46 +0530

    development changes

--- firealarm_new.sql
+++ firealarm_new.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 30, 2024 at 08:32 AM
+-- Generation Time: Dec 30, 2024 at 11:35 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `firealarm`
+-- Database: `firealarm_new`
 --
 
 -- --------------------------------------------------------
@@ -66,6 +66,56 @@ INSERT INTO `alert_notifications` (`id`, `device_id`, `device_type`, `location`,
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `assign_devices`
+--
+
+CREATE TABLE `assign_devices` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `customer_id` bigint(20) DEFAULT NULL,
+  `site_id` bigint(20) DEFAULT NULL,
+  `description` longtext DEFAULT NULL,
+  `created_ip_address` varchar(255) DEFAULT NULL,
+  `modified_ip_address` varchar(255) DEFAULT NULL,
+  `created_by` bigint(20) DEFAULT NULL,
+  `modified_by` bigint(20) DEFAULT NULL,
+  `status` enum('active','delete','inactive') NOT NULL DEFAULT 'active',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `assign_devices`
+--
+
+INSERT INTO `assign_devices` (`id`, `customer_id`, `site_id`, `description`, `created_ip_address`, `modified_ip_address`, `created_by`, `modified_by`, `status`, `created_at`, `updated_at`) VALUES
+(1, 2, 1, 'This site is assigned for you to monitor', '127.0.0.1', NULL, 1, NULL, 'active', '2024-12-19 05:29:51', '2024-12-19 05:29:51');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contacts`
+--
+
+CREATE TABLE `contacts` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `mobile` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `contacts`
+--
+
+INSERT INTO `contacts` (`id`, `name`, `mobile`, `email`, `message`, `created_at`, `updated_at`) VALUES
+(1, 'Deepak', '7318560108', 'deepak@gmail.com', 'ermkewkmktfrketgkerter', '2024-12-23 05:14:07', '2024-12-23 05:14:07');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `devices`
 --
 
@@ -90,7 +140,8 @@ CREATE TABLE `devices` (
 --
 
 INSERT INTO `devices` (`id`, `site_id`, `device_type_id`, `device_id`, `device_name`, `description`, `created_ip_address`, `modified_ip_address`, `created_by`, `modified_by`, `status`, `created_at`, `updated_at`) VALUES
-(1, 1, 1, '12222', 'Fire Alarm Pump', 'Hi This is Deepak', '127.0.0.1', NULL, 1, NULL, 'active', '2024-11-28 02:48:06', '2024-11-28 02:48:06');
+(1, 1, 1, '12222', 'Fire Alarm Pump', 'Hi This is Deepak', '127.0.0.1', NULL, 1, NULL, 'active', '2024-11-28 02:48:06', '2024-11-28 02:48:06'),
+(2, 1, 1, '12', '112', '121', '127.0.0.1', NULL, 1, NULL, 'active', '2024-12-11 01:23:57', '2024-12-11 01:23:57');
 
 -- --------------------------------------------------------
 
@@ -115,8 +166,20 @@ CREATE TABLE `device_type_masters` (
 --
 
 INSERT INTO `device_type_masters` (`id`, `device_type`, `created_ip_address`, `modified_ip_address`, `created_by`, `modified_by`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'Fire Alarm 1', '127.0.0.1', '127.0.0.1', 1, 1, 'active', '2024-11-25 03:12:36', '2024-11-26 00:20:19'),
-(2, 'Pump', '127.0.0.1', NULL, 1, NULL, 'active', '2024-11-26 00:28:20', '2024-11-26 00:28:20');
+(1, 'Fire Alarm 12', '127.0.0.1', '127.0.0.1', 1, 1, 'delete', '2024-11-25 03:12:36', '2024-12-16 04:53:48'),
+(2, 'Pump', '127.0.0.1', NULL, 1, NULL, 'active', '2024-11-26 00:28:20', '2024-11-26 00:28:20'),
+(3, 'Fire Alarm 2', '127.0.0.1', '127.0.0.1', 1, 1, 'active', '2024-12-10 03:56:33', '2024-12-10 03:56:33'),
+(7, 'aaaa', '127.0.0.1', '127.0.0.1', 1, 1, 'delete', '2024-12-12 01:01:34', '2024-12-12 01:01:34'),
+(8, '1212', '127.0.0.1', '127.0.0.1', 1, 1, 'delete', '2024-12-12 01:04:28', '2024-12-12 01:04:28'),
+(9, NULL, '127.0.0.1', '127.0.0.1', 1, 1, 'delete', '2024-12-12 01:11:04', '2024-12-12 01:11:04'),
+(10, NULL, '127.0.0.1', '127.0.0.1', 1, 1, 'delete', '2024-12-12 01:11:09', '2024-12-12 01:11:09'),
+(11, 'test', '127.0.0.1', '127.0.0.1', 1, 1, 'delete', '2024-12-12 04:31:50', '2024-12-12 04:31:50'),
+(12, 'test', '127.0.0.1', '127.0.0.1', 1, 1, 'delete', '2024-12-12 04:33:13', '2024-12-12 04:33:13'),
+(13, 'test', '127.0.0.1', '127.0.0.1', 1, 1, 'delete', '2024-12-12 04:33:29', '2024-12-12 04:33:29'),
+(14, 'ZXZXZXZX', '127.0.0.1', '127.0.0.1', 1, 1, 'delete', '2024-12-12 04:33:44', '2024-12-12 04:33:44'),
+(15, 'Fire Alarm 278', '127.0.0.1', '127.0.0.1', 1, 1, 'delete', '2024-12-16 04:54:14', '2024-12-17 03:52:56'),
+(16, 'asasas', '127.0.0.1', '127.0.0.1', 1, 1, 'delete', '2024-12-16 04:55:28', '2024-12-16 04:55:28'),
+(17, 'opiuiyouiouopiopipo', '127.0.0.1', '127.0.0.1', 1, 1, 'delete', '2024-12-17 03:53:11', '2024-12-17 03:53:11');
 
 -- --------------------------------------------------------
 
@@ -182,6 +245,7 @@ CREATE TABLE `master_admins` (
   `access_token` varchar(255) DEFAULT NULL,
   `last_login` datetime DEFAULT NULL,
   `remember_token` varchar(100) DEFAULT NULL,
+  `api_token` varchar(255) DEFAULT NULL,
   `otp` bigint(20) DEFAULT NULL,
   `status` enum('active','delete','inactive') NOT NULL DEFAULT 'active',
   `created_ip_address` varchar(255) DEFAULT NULL,
@@ -196,9 +260,10 @@ CREATE TABLE `master_admins` (
 -- Dumping data for table `master_admins`
 --
 
-INSERT INTO `master_admins` (`id`, `user_type`, `user_id`, `user_name`, `email`, `password`, `mobile_no`, `role_id`, `address`, `user_profile_image_path`, `user_profile_image_name`, `fcm_token`, `access_token`, `last_login`, `remember_token`, `otp`, `status`, `created_ip_address`, `modified_ip_address`, `created_by`, `modified_by`, `created_at`, `updated_at`) VALUES
-(1, 'system', NULL, 'Admin', 'admin@gmail.com', '$2y$10$0AVkTepXHUcEZlAqLgwPI.A3dMtsXeu9BWSXmtfEuibfb79UCY1HK', NULL, '1', NULL, NULL, NULL, NULL, NULL, '2024-11-28 04:40:40', NULL, NULL, 'active', NULL, NULL, NULL, NULL, NULL, '2024-11-27 23:10:40'),
-(2, 'system', NULL, 'Deepak', 'deepak@gmail.com', '$2y$10$H96yJAyONnwqzbJmFp5nW.sgDoE.IWT21K8/WyXsenu7p3P9Z7lHG', '7310560108', '3', 'Codepix Pune Maharastra', NULL, NULL, NULL, NULL, '2024-11-27 06:37:53', NULL, NULL, 'active', '127.0.0.1', NULL, 1, NULL, '2024-11-27 01:02:56', '2024-11-27 01:07:53');
+INSERT INTO `master_admins` (`id`, `user_type`, `user_id`, `user_name`, `email`, `password`, `mobile_no`, `role_id`, `address`, `user_profile_image_path`, `user_profile_image_name`, `fcm_token`, `access_token`, `last_login`, `remember_token`, `api_token`, `otp`, `status`, `created_ip_address`, `modified_ip_address`, `created_by`, `modified_by`, `created_at`, `updated_at`) VALUES
+(1, 'system', NULL, 'Admin', 'admin@gmail.com', '$2y$10$0AVkTepXHUcEZlAqLgwPI.A3dMtsXeu9BWSXmtfEuibfb79UCY1HK', NULL, '1', NULL, NULL, NULL, NULL, NULL, '2024-12-30 10:16:07', NULL, NULL, NULL, 'active', NULL, NULL, NULL, NULL, NULL, '2024-12-30 04:46:07'),
+(2, 'system', NULL, 'Deepak', 'deepak@gmail.com', '$2y$10$H96yJAyONnwqzbJmFp5nW.sgDoE.IWT21K8/WyXsenu7p3P9Z7lHG', '7310560108', '3', 'Codepix Pune Maharastra', NULL, NULL, NULL, NULL, '2024-12-20 04:42:17', NULL, NULL, NULL, 'active', '127.0.0.1', NULL, 1, NULL, '2024-11-27 01:02:56', '2024-12-19 23:12:17'),
+(3, 'system', NULL, 'Ashvini Bhone', 'aashubhone@gmail.com', '$2y$10$H4N.oTXGxOOLX1epyOljTeQdPHHmJkWoA0UHrLt7j8zX9vo/y4xhC', '9552246484', '4', 'vishrantWadi ,pune', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'active', '127.0.0.1', NULL, 1, NULL, '2024-12-17 06:37:24', '2024-12-17 06:37:24');
 
 -- --------------------------------------------------------
 
@@ -228,7 +293,9 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (9, '2024_11_25_043851_create_alert_notifications_table', 2),
 (11, '2024_11_25_070606_create_device_type_masters_table', 3),
 (12, '2024_11_26_063603_create_site_masters_table', 4),
-(13, '2024_11_28_060802_create_devices_table', 5);
+(13, '2024_11_28_060802_create_devices_table', 5),
+(14, '2024_12_16_091649_create_assign_devices_table', 6),
+(15, '2024_12_23_095058_create_contacts_table', 7);
 
 -- --------------------------------------------------------
 
@@ -309,7 +376,7 @@ CREATE TABLE `role_privileges` (
 --
 
 INSERT INTO `role_privileges` (`id`, `role_name`, `privileges`, `created_ip_address`, `modified_ip_address`, `created_by`, `modified_by`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'Super Admin', 'dashboard_view, device_type_master_view, site_master_view, role_privileges_view, user_view, device_view, map_site_view, alarm_view, report_view, device_master_add, site_master_add, role_privileges_add, user_add, device_add, map_site_add, alarm_add, report_add, device_master_edit, site_master_edit, role_privileges_edit, user_edit, device_edit, map_site_edit, alarm_edit, report_edit, device_type_master_delete, site_master_delete, role_privileges_delete, user_delete, device_delete, map_site_delete, alarm_delete, report_delete, device_type_master_status_change, site_master_status_change, role_privileges_status_change, user_status_change, device_status, map_site_status, alarm_status, report_status_change', NULL, '127.0.0.1', NULL, 1, 'active', NULL, '2024-11-25 03:53:55'),
+(1, 'Super Admin', 'dashboard_view, device_type_master_view, site_master_view, device_type_masterrole_privileges_view, user_view, device_view, map_site_view, alarm_view, report_view, device_master_add, site_master_add, role_privileges_add, user_add, device_add, map_site_add, alarm_add, report_add, device_master_edit, site_master_edit, role_privileges_edit, user_edit, device_edit, map_site_edit, alarm_edit, report_edit, device_type_master_delete, site_master_delete, role_privileges_delete, user_delete, device_delete, map_site_delete, alarm_delete, report_delete, device_type_master_status_change, site_master_status_change, role_privileges_status_change, user_status_change, device_status, map_site_status, alarm_status, report_status_change,device_type_master_edit', NULL, '127.0.0.1', NULL, 1, 'active', NULL, '2024-11-25 03:53:55'),
 (3, 'Admin', 'dashboard_view,device_type_master_view,device_master_add,device_master_edit,device_type_master_delete,device_type_master_status_change,site_master_view,site_master_add,site_master_edit,site_master_delete,site_master_status_change,role_privileges_view,role_privileges_add,role_privileges_edit,role_privileges_delete,role_privileges_status_change,user_view,user_add,user_edit,user_delete,user_status_change,device_view,device_add,device_edit,device_delete,device_status,map_site_view,map_site_add,map_site_edit,map_site_delete,map_site_status,alarm_view,alarm_edit,alarm_add,alarm_delete,alarm_status,report_view,report_add', '127.0.0.1', '127.0.0.1', 1, 1, 'active', '2024-11-15 05:28:04', '2024-11-27 01:36:03'),
 (4, 'Client Admin', 'dashboard_view,alarm_view,reports_view,reports_view', '127.0.0.1', '127.0.0.1', 1, 1, 'active', '2024-11-15 05:32:08', '2024-11-26 04:46:26'),
 (5, 'Client Operator', 'dashboard_view,device_type_master_view,device_master_add,device_master_edit,device_type_master_delete,device_type_master_status_change,site_master_view,site_master_add,site_master_edit,site_master_delete,site_master_status_change,role_view,role_view,role_add,role_edit,role_delete,role_status,system_user_view,system_user_add,system_user_edit,system_user_delete,role_status,device_management_view,device_view,device_add,device_edit,device_delete,device_status,map_site_view,map_site_add,map_site_edit,map_site_delete,map_site_status,alarm_view,alarm_edit,alarm_add,alarm_delete,alarm_status,reports_view,reports_edit,reports_view,reports_delete,reports_status', '127.0.0.1', NULL, 1, NULL, 'active', '2024-11-26 04:47:07', '2024-11-26 04:47:07');
@@ -338,8 +405,11 @@ CREATE TABLE `site_masters` (
 --
 
 INSERT INTO `site_masters` (`id`, `site_name`, `site_address`, `created_ip_address`, `modified_ip_address`, `created_by`, `modified_by`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'Codepix  11', 'Swami Plot No 166, Gajanan Nagar, 1/1/1, opposite ASHTAVINAYAK CITY, next to Pearl Society, Maharashtra Vidhyut Department Quarters, Phursungi, Pune, Maharashtra 412308', '127.0.0.1', '127.0.0.1', 1, 1, 'active', '2024-11-26 01:31:05', '2024-11-26 04:33:13'),
-(2, NULL, NULL, '127.0.0.1', NULL, 1, NULL, 'active', '2024-11-28 02:46:18', '2024-11-28 02:46:18');
+(1, 'Codepix  111', 'Swami Plot No 166, Gajanan Nagar, 1/1/1, opposite ASHTAVINAYAK CITY, next to Pearl Society, Maharashtra Vidhyut Department Quarters, Phursungi, Pune, Maharashtra 412308', '127.0.0.1', '127.0.0.1', 1, 1, 'active', '2024-11-26 01:31:05', '2024-12-11 04:15:43'),
+(2, NULL, NULL, '127.0.0.1', '127.0.0.1', 1, 1, 'delete', '2024-11-28 02:46:18', '2024-11-28 02:46:18'),
+(3, 'aa', 'aa', '127.0.0.1', '127.0.0.1', 1, 1, 'delete', '2024-12-12 03:59:49', '2024-12-12 03:59:49'),
+(4, 'ashu', 'Swami Plot No 166, Gajanan Nagar, 1/1/1, opposite ASHTAVINAYAK CITY, next to Pearl Society, Maharashtra Vidhyut Department Quarters, Phursungi, Pune, Maharashtra 412308', '127.0.0.1', '127.0.0.1', 1, 1, 'active', '2024-12-12 23:53:36', '2024-12-13 00:08:57'),
+(5, 'asas', 'asasas', '127.0.0.1', '127.0.0.1', 1, 1, 'delete', '2024-12-13 01:07:47', '2024-12-13 01:07:47');
 
 -- --------------------------------------------------------
 
@@ -391,6 +461,18 @@ CREATE TABLE `visual_settings` (
 -- Indexes for table `alert_notifications`
 --
 ALTER TABLE `alert_notifications`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `assign_devices`
+--
+ALTER TABLE `assign_devices`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contacts`
+--
+ALTER TABLE `contacts`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -487,16 +569,28 @@ ALTER TABLE `alert_notifications`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
+-- AUTO_INCREMENT for table `assign_devices`
+--
+ALTER TABLE `assign_devices`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `contacts`
+--
+ALTER TABLE `contacts`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `devices`
 --
 ALTER TABLE `devices`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `device_type_masters`
 --
 ALTER TABLE `device_type_masters`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -514,13 +608,13 @@ ALTER TABLE `general_settings`
 -- AUTO_INCREMENT for table `master_admins`
 --
 ALTER TABLE `master_admins`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -544,7 +638,7 @@ ALTER TABLE `role_privileges`
 -- AUTO_INCREMENT for table `site_masters`
 --
 ALTER TABLE `site_masters`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
